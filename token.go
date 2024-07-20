4ee1ca212a5d255f0000b3e29edf1299712be36e
Agustin RODRIGUEZ <a.rodriguez@naranja.com>
2024-07-20 14:54:18 -0300

    Add proper documentation

--- token.go
+++ token.go
@@ -1,5 +1,7 @@
 package xpay
 
+// NewXPayToken constructs a new X-Pay Token which can be use to make
+// requests to Visa API endpoints.
 func NewXPayToken(tc *TokenConfig) Token {
 	return tc.GenerateXPayToken()
 }
--- types.go
+++ types.go
@@ -10,8 +10,10 @@ import (
 	"time"
 )
 
+// Aliasing string to use for Token return.
 type Token string
 
+// TokenConfig is the type of constructor options for NewXPayToken(...).
 type TokenConfig struct {
 	SharedSecret string
 	DateTime     time.Time
@@ -20,10 +22,13 @@ type TokenConfig struct {
 	PostBody     string
 }
 
+// Timestamp parses time.Time type into a string representix Unix timestamp.
 func (tc TokenConfig) Timestamp() string {
 	return strconv.Itoa(int(tc.DateTime.Unix()))
 }
 
+// GetMessage return a string containing each element of the message that is
+// digested later as part of the specification.
 func (tc TokenConfig) GetMessage() string {
 	return strings.Join(
 		[]string{
@@ -35,12 +40,15 @@ func (tc TokenConfig) GetMessage() string {
 	)
 }
 
+// HashString digest the message using sha256 and hmac.
 func (tc TokenConfig) HashString() string {
 	h := hmac.New(sha256.New, []byte(tc.SharedSecret))
 	h.Write([]byte(tc.GetMessage()))
 	return hex.EncodeToString(h.Sum(nil))
 }
 
+// GenerateXPayToken each element on the token together and returns
+// a string of type Token.
 func (tc *TokenConfig) GenerateXPayToken() Token {
 	token := strings.Join(
 		[]string{
